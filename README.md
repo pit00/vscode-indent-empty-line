ac2af2039ba906365b7dbdd3aaee463d22638edc
pit00 <pitu.danilo@gmail.com>
2022-06-13 03:15:41 -0300

    initial

--- README.md
+++ README.md
@@ -1,20 +1,9 @@
 # vscode-indent-on-empty-line README
 
-When the cursor moves to an empty line, automatically indent to the correct level.
-
-That is, moving the cursor onto an empty line with the up/down/left/right arrows will cause the cursor to also jump to an appropriate level of indentation. This is influenced by the indentation settings for the language you're working in, but falls back to using the indentation level of the next non-empty line.
+Indent a empty line to the correct level.
 
 ## Configuration
 
-This plugin adds the following movement commands and binds them to the arrow keys by default, overriding the corresponding builtin movement commands:
+This plugin adds the followingcommands and binds to "ctrl ctrl" by default:
 
-- `implicit-indent.cursorUp`
-- `implicit-indent.cursorDown`
-- `implicit-indent.cursorLeft`
-- `implicit-indent.cursorRight`
-
-It also binds to the following events
-
-- `onStartupFinished`
-
-If you wish to use different keys for these (such as vim-style keys), you can rebind those commands to different keys in your personal keyboard settings.
+- `implicit-indent.trigger`
--- src/extension.ts
+++ src/extension.ts
@@ -17,49 +17,25 @@ export function activate(context: vscode.ExtensionContext) {
   }
 
   // Define some movement commands, registered with the implicit-indent prefix.
-  const moveCommands = ["cursorUp", "cursorDown", "cursorLeft", "cursorRight"];
-  moveCommands.forEach((moveCommand) => {
-    const name = `implicit-indent.${moveCommand}`;
-    const disposable = vscode.commands.registerCommand(name, async () => {
-      // Get the cursor's current line and check if it is empty.
-      const editor = vscode.window.activeTextEditor!;
-      const cursorPos = editor.selection.start;
-      const cursorLine = cursorPos.line;
-      await executeIndent(editor, cursorPos, cursorLine, moveCommand);
-    });
-    context.subscriptions.push(disposable);
+  const name = `implicit-indent.trigger`;
+  const disposable = vscode.commands.registerCommand(name, async () => {
+    // Get the cursor's current line and check if it is empty.
+    const editor = vscode.window.activeTextEditor!;
+    const cursorPos = editor.selection.start;
+    const cursorLine = cursorPos.line;
+    await executeIndent(editor, cursorPos, cursorLine);
   });
-
-  vscode.window.onDidChangeTextEditorSelection(
-    async (event: TextEditorSelectionChangeEvent) => {
-      // Get the cursor's current line and check if it is empty.
-      const editor = vscode.window.activeTextEditor!;
-      const cursorPos = editor.selection.start;
-      const cursorLine = cursorPos.line;
-      if (previous_line_number_from_click == cursorLine) {
-        return;
-      }
-
-      await executeIndent(editor, cursorPos, cursorLine, "");
-      previous_line_number_from_click = cursorLine;
-    }
-  );
+  context.subscriptions.push(disposable);
 }
 
 async function executeIndent(
   editor: vscode.TextEditor,
   cursorPos: Position,
-  cursorLine: number,
-  moveCommand: string
+  cursorLine: number
 ) {
   // First, wait to acquire the lock before doing anything.
   const releaseLock = await lock.acquire();
   try {
-    // Execute the underlying movement command associated with this command.
-    if (moveCommand) {
-      await vscode.commands.executeCommand(moveCommand);
-    }
-
     let cursorLineText = editor.document.lineAt(cursorLine).text;
     if (cursorLineText === "") {
       // Find the next line that is not empty or whitespace-only.
